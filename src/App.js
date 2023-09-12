7f5836b6f242f78375955d93f3d1525b57ad43a7
BrendonLeeYiHao <brendonlee0413@gmail.com>
2023-09-12 16:20:50 +0800

    Add routing

--- src/App.js
+++ src/App.js
@@ -5,40 +5,23 @@ import ListGroup from './ListGroup';
 import PassFunction from './PassFunction';
 
 //
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, createBrowserRouter, Outlet } from 'react-router-dom';
 import CreateUser from './Components/CreateUser';
 import ListUser from './Components/ListUser';
 import EditUser from './Components/EditUser';
 import TopNavigationBar from './TopNavigationBar';
 
 function App() {
+//   <Routes>
+//   <Route index element={<ListUser/>}/>
+//   <Route path="user/create" element={<CreateUser/>}/>
+//   <Route path="message" element={<Message/>} />
+//   <Route path="login" element={<Login/>}/>
+// </Routes>
+
   return (
-    <div style={{textAlign:'center'}}>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
-      {/* <h1>Hello this is my first react app.</h1>
-      <h2>It's my first time learning react</h2>
-      <ListGroup/>
-      <PassFunction msg="Message here hehehehhe">
-        <h2>React is a library not a <u>Framework</u></h2>
-        <span><b>Testing 12345</b></span>
-      </PassFunction> */}
-      <TopNavigationBar/>
-
-    </div>
+    <Outlet/>
+    // <TopNavigationBar/>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,18 +5,63 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import { RouterProvider, createBrowserRouter, useNavigate } from 'react-router-dom';
+import ListUser from './Components/ListUser';
+import Login from './Components/Login';
+import GuestLayout from './Components/Guest/GuestLayout';
+import Home from './Components/Guest/Home';
+import CreateUser from './Components/CreateUser';
+import AdminLayout from './Components/Admin/AdminLayout';
+import Message from './Message';
+import Dashboard from './Components/Admin/Dashboard';
+import { AuthProvider } from './AuthContext';
 
+// import { isAuthenticated } from './Components/Login';
+// import { AuthProvider } from './AuthContext';
 
-// import 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-// import 'bootstrap/dist/js/bootstrap.bundle';
+// const isAuthenticated = false;
+
+// function ProtectRoute(element){
+//   const navigate = useNavigate();
+
+//   if(!isAuthenticated){
+//     navigate('/login');
+//     return null;
+//   }
+//   return element;
+// }
+
+const router = createBrowserRouter([
+  {
+    path: '',
+    element: <GuestLayout/>,
+    children: [
+      {path: '', element: <Home/>},
+      {path: 'register', element: <CreateUser/>},
+      {path: 'account-update-delete', element: <ListUser/>},
+      {path: 'login', element: <Login/>},
+      {path: 'message', element: <Message/>}
+    ]
+  },
+  {
+    path: 'admin',
+    element: <AdminLayout/>,
+    children: [
+      {path: '', element: <Dashboard/>},
+      {path: 'message', element: <Message/>}
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
+    </AuthProvider>
+
   </React.StrictMode>
 );
 
